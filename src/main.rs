7859f71cc1b369be72b27888662097c7b36f6440
Fernando Costa Migliorini <fercosmig@gmail.com>
2024-04-27 02:40:19 -0300

    media_mediana_moda

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,13 @@ use std::collections::HashMap;
 fn media(vetor: &Vec<i32>) -> f32
 {
     let mut soma: i32 = 0;
-    let quantidade_itens: usize = vetor.len().try_into().unwrap();
-    let resultado: f32;
 
     for item in vetor
     {
         soma += item;
     }
 
-    resultado = soma as f32 / quantidade_itens as f32;
-    return resultado;
+    soma as f32 / vetor.len() as f32
 }
 
 fn mediana(vetor: &Vec<i32>) -> f32
@@ -58,24 +55,12 @@ fn moda(vetor: &Vec<i32>) -> i32
     return maior_key;
 }
 
-fn soma_itens(vetor: &Vec<i32>) -> i32
-{
-    let mut soma: i32 = 0;
-    for item in vetor
-    {
-        soma += item;
-    }
-    return soma;
-}
-
 fn main()
 {
     {
         let valores: Vec<i32> = vec![8, 7, 3, 1, 5, 7, 4, 9, 2];
 
-        println!("Meu vetor: {:?}", valores);
-        println!("Quantidade de itens: {}", valores.len());
-        println!("Soma dos itens: {}", soma_itens(&valores));
+        println!("Meu vetor[{}]: {:?}", valores.len(), valores);
         println!("Média dos itens: {}", media(&valores));
         println!("Mediana: {}", mediana(&valores));
         println!("Moda: {}", moda(&valores));
@@ -89,9 +74,7 @@ fn main()
     {
         let valores: Vec<i32> = vec![4, 2, 1, 5, 0, 4, 1, 3, 6, 4];
 
-        println!("Meu vetor: {:?}", valores);
-        println!("Quantidade de itens: {}", valores.len());
-        println!("Soma dos itens: {}", soma_itens(&valores));
+        println!("Meu vetor[{}]: {:?}", valores.len(), valores);
         println!("Média dos itens: {}", media(&valores));
         println!("Mediana: {}", mediana(&valores));
         println!("Moda: {}", moda(&valores));
